988708f083e3026e1f495417b6158c329dfbd93e
Roberto Carlos Ricci <roberto.rricci@gmail.com>
2023-02-03 12:42:42 -0300

    Update dw_faturamento_ano.sql

--- models/prod/dw_faturamento_ano.sql
+++ models/prod/dw_faturamento_ano.sql
@@ -1,7 +1,7 @@
 with dw_faturamento_ano as (
     select DATE_PART('year', TO_DATE( purchased_at) ) AS "Ano"
         ,sum(ap.price) as "Faturamento"
-    from file.purchases pur inner join file.products ap
+    from dbt.purchases pur inner join dbt.products ap
     on pur.product_id = ap.id
     group by 1
     order by 1
