f1fc4333fd57e1809fa506f030a8b80b8fa00672
roberto ricci <roberto.rricci@gmail.com>
2023-02-03 17:22:04 -0300

    add

---
+++ models/normalized/tab_normalizada.sql
@@ -0,0 +1,12 @@
+
+with tab_normalizada as (
+     select
+        cast(JSON_EXTRACT_PATH_TEXT("_AIRBYTE_DATA",'id') as numeric) as "id",
+        cast(JSON_EXTRACT_PATH_TEXT("_AIRBYTE_DATA",'make') as varchar) as "make",
+        cast(JSON_EXTRACT_PATH_TEXT("_AIRBYTE_DATA",'year') as numeric) as "year",
+        cast(JSON_EXTRACT_PATH_TEXT("_AIRBYTE_DATA",'model') as varchar) as "model",
+        cast(JSON_EXTRACT_PATH_TEXT("_AIRBYTE_DATA",'price') as float) as "price"
+    from desenv._airbyte_raw_products
+)
+select * from tab_normalizada
+
