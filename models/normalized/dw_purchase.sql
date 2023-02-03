d566636b0d3b3530366db40c30d7116baad68e9c
Roberto Carlos Ricci <roberto.rricci@gmail.com>
2023-02-03 12:43:17 -0300

    Update dw_purchase.sql

--- models/normalized/dw_purchase.sql
+++ models/normalized/dw_purchase.sql
@@ -1,7 +1,7 @@
 with dw_purchase as (
     select pro.MAKE ,
      pro.YEAR ,pro.MODEL ,pro.PRICE,pur.ID,pur.ADDED_TO_CART_AT ,pur.PURCHASED_AT
-    from file.purchases pur inner join file.products pro
+    from dbt.purchases pur inner join dbt.products pro
     on pur.product_id = pro.id
     order by 2 desc
 )
